4c619a0f88f2f405fb8da60d86b5b6c2a82eefa9
Karan <brarkaran938@gmail.com>
2022-06-26 15:24:13 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
+    <title>The Store</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            The Store
+        </header>
+
+        <div id="products">
+            <div class="product-card"></div>
+            <div class="product-card"></div>
+            <div class="product-card"></div>
+            <div class="product-card"></div>
+            <div class="product-card"></div>
+            <div class="product-card"></div>
+        </div>
+
+        <button id="cartButton">View cart</button>
+
+        <div id="cart">
+
+        </div>
+
+        <div id="order-form">
+
+        </div>
+
+        <footer>
+
+        </footer>
+    </div>
+</body>
+</html>
